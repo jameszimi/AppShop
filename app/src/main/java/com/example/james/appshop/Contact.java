7c5712f2c0a25b66efee2c393db86e8b3e42adc2
jameszimi <36562063+jameszimi@users.noreply.github.com>
2018-02-25 10:45:02 +0700

    newver

---
+++ app/src/main/java/com/example/james/appshop/Contact.java
@@ -0,0 +1,83 @@
+package com.example.james.appshop;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Contact extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.nav_home:
+                Intent h = new Intent(Contact.this, MainActivity.class);
+                startActivity(h);
+                break;
+            case R.id.nav_login:
+                Intent l = new Intent(Contact.this, Login.class);
+                startActivity(l);
+                break;
+            case R.id.nav_check:
+                Intent ch = new Intent(Contact.this, Check.class);
+                startActivity(ch);
+                break;
+            case R.id.nav_cate:
+                Intent ca = new Intent(Contact.this, Cast.class);
+                startActivity(ca);
+                break;
+            //case R.id.nav_contact:
+            //    Intent co = new Intent(Contact.this, Contact.class);
+            //    startActivity(co);
+            //    break;
+            case R.id.nav_about:
+                Intent ab = new Intent(Contact.this, About.class);
+                startActivity(ab);
+                break;
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
