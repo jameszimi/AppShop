786bfd5ca979f45cb9e86f907a6a9c94e3c194bf
jameszimi <36562063+jameszimi@users.noreply.github.com>
2018-03-30 13:35:20 +0700

    Version Beta 1.2.3.3 Note Patch Version Beta 1.2.3.3 - ได้ทำการเพิ่มหน้าสมัครสมาชิกเข้ามา - เพิ่มหน้าสมัครสมาชิก สามารถกดได้ที่หน้า login - เพิ่มหน้าลืมรหัสผ่าน - เปลี่ยนสีปุ่มหน้า login - เปลี่ยนสีปุ่มหน้าลืมรหัสผ่าน - เพิ่ม Event ปุ่ม "ยกเลิก" หน้าลืมรหัสผ่านให้กลับมาหน้า Login ใหม่อีกรอบ - เพิ่ม และกำหนด Event ปุ่ม "ยกเลิก" หน้าสมัครสมาชิกให้สามารถกลับมายังหน้า Login bug - หน้าสมัครสมาชิกไม่ขึ้นหน้าว่า สมัครสมาชิก แต่ขึ้นเป็นชื่อแอพแทน - การจัดวางหน้าต่างแอพพลิเคชั่นไม่สมบูรณ์ในหน้าสมัครสมาชิก ระห่างยังไม่สวยเท่าที่ควร - Navigationview เมื่อกดกลับจะแสดงแถบที่เคยเลือกไปรอบที่แล้ว Note Patch Version Beta 1.2.3.1 - เพิ่ม Hot bar เข้ามาแต่ยังใช้ไม่ได้ - เพิ่ม หน้า Login ให้สามารถใช้ได้ - อัพเดทให้ Hot bar ในหน้าหลักสามารถใช้งานได้ - เปลี่ยนสีช่องใส่ข้อความเป็นสีฟ้าเข้ม

--- app/src/main/java/com/example/james/appshop/Signup.java
+++ app/src/main/java/com/example/james/appshop/Signup.java
@@ -1,7 +1,10 @@
 package com.example.james.appshop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Signup extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class Signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        Button btnCancle = (Button) findViewById(R.id.cancle);
+        btnCancle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent callback = new Intent(Signup.this, Login.class);
+                startActivity(callback);
+                finish();
+            }
+        });
     }
 }
